4ad4b4c1543841c305212e1bdcaef47569bc96ae
ranaahmed <rnaahmd240@gmail.com>
2025-09-22 09:39:03 +0300

    Function arrangement

---
+++ mybank/bank.py
@@ -0,0 +1,25 @@
+
+class Bank:
+    def add_customer():
+        pass
+    def find_customer():
+        pass
+    def withdraw():
+        pass
+    def deposit(self):
+        pass
+    def transfer():
+        pass
+    def save_to_csv():
+        pass
+    def load_from_csv():
+        pass
+        
+
+
+
+
+
+
+
+        
---
+++ mybank/customer.py
@@ -0,0 +1,5 @@
+class Customer:
+     def add_account(): 
+        # im gonna make the user create acc
+        pass 
+
