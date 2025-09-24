59606fb26584e5193df1bc542040b88ae19d0ebd
ranaahmed <rnaahmd240@gmail.com>
2025-09-24 12:40:44 +0300

    finally ID works UPDATE

--- mybank/bank.py
+++ mybank/bank.py
@@ -7,16 +7,21 @@ class Bank:
     def generate_customer_id(self): #copied from conor
        if not self.customers:
         return "10001"
-       max_id = max(int(customer.customer_id) for customer in self.customers)
-       return str(max_id + 1) 
+       ids = [int(customer.customer_id) for customer in self.customers if customer.customer_id and customer.customer_id.isdigit()]
+       if not ids:   # to make sure
+        return "10001"
+       max_id = max(ids)
+       return str(max_id + 1)
 
-    def add_customer(self,name,balance=0):
-      customer = Customer(name, customer_id, balance)
+    def add_customer(self,name,balance=0,customer_id=None,username=None):
+      if customer_id is None:
+          customer_id =self.generate_customer_id()
+      customer = Customer(name,customer_id,balance,username)
       self.customers.append(customer)
       print (f" Customer {name} added with ID {customer_id}")
       return customer
     def find_customer(self,customer_id): 
-          for customer  in self.customers :
+          for customer in self.customers :
              if customer.customer_id == customer_id:
                 print('Customer found',customer.name)
                 return customer
@@ -39,16 +44,20 @@ class Bank:
        with open('customers.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-           balance = float(row['balance']) if row['balance'] else 0
-           customer = Customer(row['name'], row['customer_id'], balance) # i saw this in tiketok!
-           self.customers.append(customer)
-import csv 
+           try:
+            balance = float(row['balance']) if row['balance'] else 0
+            customer = Customer(row['name'], row['customer_id'], balance) # i saw this in tiketok!
+            self.customers.append(customer)
+           except ValueError:
+              print('Error loading customer')
+
 class Customer:
     # copied the idea from stackoverflow
-   def __init__(self,name,customer_id,balance=0):
+   def __init__(self,name,customer_id,balance=0,username=None):
         self.name = name
         self.customer_id = customer_id
         self.balance = balance
+        self.username= username
         self.accounts = {
             'saving': Account('saving',balance,customer_id),
             'checking': Account('checking',0,customer_id)
@@ -59,8 +68,7 @@ class Customer:
             if self.accounts[from_acc].withdraw(amount):
                 self.accounts[to_acc].deposit(amount)
                 return True
-        return False
-import csv 
+        return False 
 class Account:
     def __init__(self,account_type,balance,account_number):
         self.account_type =account_type
@@ -99,12 +107,13 @@ def main():
       print('1-Login to Account:')
       print('2-Create an Account:')
       print('3-Exit')
-      choice = input('Enter your chpice(1-3):')
+      choice = input('Enter your choice(1-3):')
       if choice =='1':
-         username = input('Enter customer username: ')
-         customer = bank.find_customer(username)
+         customer_id = input('Enter customer ID: ')
+         customer = bank.find_customer(customer_id)
          if customer:
             while True:
+                print(' Welcome!', customer.name)
                 print (' 1-Deposit 2-Withdraw 3-Transfer 4-Exit')
                 op =input('Choose:')
                 if op == '1':
@@ -127,9 +136,11 @@ def main():
          bank.save_to_csv()
       elif choice =='2':
          name = input('Enter customer name: ')
-         username = input('Enter customer username: ')
+         username = input('Enter username: ')
          balance = float(input('Enter initial balance: ') or 0)
-         bank.add_customer(name, username, balance)
+         new_customer= bank.add_customer(name, balance,username=username)
+         print(f"Welcome {new_customer.name}, this is your username: {new_customer.username}, "
+      f"and this is your ID: {new_customer.customer_id} with balance {balance}")
          bank.save_to_csv()
       elif choice=='3':
          is_running= False
@@ -139,49 +150,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-# ( THIS IM GONNA WORK ON IT TOMMOROR)
-# if __name__ == "__main__":
-#    main()
-# 
-       
-       # return here after i create login and create account
-
-#         self.checking_balance=0
-# customers =[]
-# def createaccount():
-#     names = input (' enter your first name:')
-#     usernames = input (' enter a username:')
-#     passwords = input('enter a password:')
-#     print(' choose account type:')
-#     print(' 1-saving account, 2- checking account')
-#     choose = input(' enter 1 or 2')
-#     if choose == '1':
-#         acctype = 'saving'
-#     else:
-#         acctype = 'checking'
-#     customer =Customer(names,usernames,passwords,acctype)
-#     customers.append(customer)
-# def login():
-#     username_input = input("Enter your username: ")
-#     password_input = input("Enter your password: ")
-#     for customer in customers:
-#         if customer.usernames == username_input and customer.passwords == password_input:
-#             print("Welcome")
-#             return customer
-#     print("Incorrect username or password!")
-#     return
-           
-                         
-
-
-
 
         
