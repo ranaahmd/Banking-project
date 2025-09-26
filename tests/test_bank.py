86cefaee8109ff0a73ec7ecf068a5b66c5e810bf
ranaahmed <rnaahmd240@gmail.com>
2025-09-26 13:15:54 +0300

    test generate_customer_id

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,2 +1,8 @@
 import unittest
 from mybank.bank import Bank
+from mybank.bank import Customer
+from mybank.bank import Account
+class Testbank(unittest.TestCase):
+    def test_generate_customer_id(self):
+        customer_id = (10007,'Rafa')
+        self.assertTrue(customer_id, True)
